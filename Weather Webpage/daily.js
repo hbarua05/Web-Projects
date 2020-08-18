be6a6a42e5fb27d57eaea829ef11f4e76cc3884f
Hrithik barua <66213858+hbarua05@users.noreply.github.com>
2020-08-18 09:12:37 +0600

    Update daily.js

--- Weather Webpage/daily.js
+++ Weather Webpage/daily.js
@@ -22,7 +22,7 @@ function alertError() {
 }
 
 function getDailyWeather(lat, lon) {
-    const URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=674d13d1e2dffb08a8f5829bf90d68d0`;
+    const URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid={OpenWeatherMapAPIkey}`;
 
     fetch(URL)
         .then((response) => response.json())
